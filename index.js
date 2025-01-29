6d23ccf7a41ac83014620ebb6dbd9f5fcccdbdca
mehediNabil <mehedinabil13@gmail.com>
2025-01-30 00:56:54 +0600

    added cart api

--- index.js
+++ index.js
@@ -1,11 +1,75 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yitxt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+      // Connect the client to the server	(optional starting in v4.7)
+      const menuCollection = client.db('bistroDb').collection('menu');
+      const reviewCollection = client.db('bistroDb').collection('reviews');
+      const cartCollection = client.db('bistroDb').collection('carts');
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+// menu api
+    app.get('/menu', async(req,res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result)
+    })
+
+    // review api
+    app.get('/reviews', async(req,res)=>{
+        const result = await reviewCollection.find().toArray();
+        res.send(result)
+    })
+    // cart collection
+
+    app.get('/carts', async(req,res)=>{
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/carts', async(req,res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
+    })
+    app.delete('/carts/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
+    
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req,res)=>{
     res.send("Boss is Running")
